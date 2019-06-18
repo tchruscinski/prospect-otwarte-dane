d090f89d496134d1718bf564f386a806a5f6c7a8
krzysztof <krzysztof.brzoska@ibm.com>
2019-06-18 14:44:19 +0200

    new route added /data/<yourValue>

--- hackathon-backend/api/routes/data.js
+++ hackathon-backend/api/routes/data.js
@@ -9,12 +9,22 @@ router.get('/', (req, res, next) => {
     const jobsInfo = new jobsOffersInfo(100);
     connection.query(jobsInfo.createSelectQuery(), function (error, results, fields) {
         if (error) throw error;
-        console.log('The solution is: ', results);
         res.status(200).json({
             data: results
         });
-      });
-    
+    });   
+});
+
+router.get('/:amountOfJobOffers', (req, res, next) => {
+    const jobsInfo = new jobsOffersInfo(req.params.amountOfJobOffers);
+    connection.query(jobsInfo.createSelectQuery(), function (error, results, fields) {
+        if (error) throw error;
+        res.status(200).json({
+            message: "pobrales rekordy w których 'oferty_internet' >= " + req.params.amountOfJobOffers,
+            data: results,
+            yourValue: req.params.amountOfJobOffers
+        });
+    });
 });
 
 module.exports = router;
