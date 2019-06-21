db3bbc3a143ed3ffc3ec592479ef585a929f4e48
krzysztof <krzysztof.brzoska@ibm.com>
2019-06-22 01:57:58 +0200

    wsp matiego added

--- hackathon-backend/api/services/cityData.js
+++ hackathon-backend/api/services/cityData.js
@@ -4,22 +4,23 @@ class cityData {
         this.dataArray = [];
     }
 
-    createSelectQueryAllCities(){
-        let sql = `SELECT nieruchomosci.miasto, AVG(nieruchomosci.cena) AS cenam2, SUM(bezrobotni.ilosc) AS bezrobotni, wynagrodzenie.wynagrodzenie_brutto, wynagrodzenie.wynagrodzenie_w_relacji, populacja.liczba_ludnosci, populacja.procent_bezrobocia, populacja.ilosc_ofert_pracy, populacja.zatrudnieni_rolnictwo, populacja.zatrudnieni_uslugi, populacja.zatrudnieni_przemysl FROM bezrobotni INNER JOIN nieruchomosci ON bezrobotni.miasto = nieruchomosci.miasto INNER JOIN wynagrodzenie ON wynagrodzenie.miasto = bezrobotni.miasto INNER JOIN populacja ON populacja.miasto = bezrobotni.miasto GROUP BY populacja.liczba_ludnosci, wynagrodzenie.wynagrodzenie_brutto, wynagrodzenie.wynagrodzenie_w_relacji, nieruchomosci.miasto, populacja.procent_bezrobocia, populacja.ilosc_ofert_pracy, populacja.zatrudnieni_rolnictwo, populacja.zatrudnieni_uslugi, populacja.zatrudnieni_przemysl`;
+    createSelectQueryAllCities() {
+        let sql = `SELECT nieruchomosci.miasto, AVG(nieruchomosci.cena) AS cenam2, SUM(bezrobotni.ilosc) AS bezrobotni, wynagrodzenie.wynagrodzenie_brutto, wynagrodzenie.wynagrodzenie_w_relacji, populacja.liczba_ludnosci, populacja.procent_bezrobocia, populacja.ilosc_ofert_pracy, populacja.zatrudnieni_rolnictwo, populacja.zatrudnieni_uslugi, populacja.zatrudnieni_przemysl, populacja.liczba_ludnosci_stara, populacja.srednia_pensja_stara, populacja.srednia_pensja FROM bezrobotni INNER JOIN nieruchomosci ON bezrobotni.miasto = nieruchomosci.miasto INNER JOIN wynagrodzenie ON wynagrodzenie.miasto = bezrobotni.miasto INNER JOIN populacja ON populacja.miasto = bezrobotni.miasto GROUP BY populacja.liczba_ludnosci, wynagrodzenie.wynagrodzenie_brutto, wynagrodzenie.wynagrodzenie_w_relacji, nieruchomosci.miasto, populacja.procent_bezrobocia, populacja.ilosc_ofert_pracy, populacja.zatrudnieni_rolnictwo, populacja.zatrudnieni_uslugi, populacja.zatrudnieni_przemysl, populacja.liczba_ludnosci_stara, populacja.srednia_pensja_stara, populacja.srednia_pensja`;
         return sql;
     }
 
-    createSelectQuery(){
-        let sql = `SELECT AVG(nieruchomosci.cena) AS cenam2, SUM(bezrobotni.ilosc) AS bezrobotni, wynagrodzenie.wynagrodzenie_brutto, wynagrodzenie.wynagrodzenie_w_relacji, populacja.liczba_ludnosci, populacja.procent_bezrobocia, populacja.ilosc_ofert_pracy, populacja.zatrudnieni_rolnictwo, populacja.zatrudnieni_uslugi, populacja.zatrudnieni_przemysl FROM bezrobotni INNER JOIN nieruchomosci ON bezrobotni.miasto = nieruchomosci.miasto INNER JOIN wynagrodzenie ON wynagrodzenie.miasto = bezrobotni.miasto INNER JOIN populacja ON populacja.miasto = bezrobotni.miasto WHERE bezrobotni.miasto = '${this.name}' GROUP BY populacja.liczba_ludnosci, populacja.procent_bezrobocia, populacja.ilosc_ofert_pracy, populacja.zatrudnieni_rolnictwo, populacja.zatrudnieni_uslugi, populacja.zatrudnieni_przemysl`;
+    createSelectQuery() {
+        let sql = `SELECT AVG(nieruchomosci.cena) AS cenam2, SUM(bezrobotni.ilosc) AS bezrobotni, wynagrodzenie.wynagrodzenie_brutto, wynagrodzenie.wynagrodzenie_w_relacji, populacja.liczba_ludnosci, populacja.procent_bezrobocia, populacja.ilosc_ofert_pracy, populacja.zatrudnieni_rolnictwo, populacja.zatrudnieni_uslugi, populacja.zatrudnieni_przemysl, populacja.liczba_ludnosci_stara, populacja.srednia_pensja_stara, populacja.srednia_pensja FROM bezrobotni INNER JOIN nieruchomosci ON bezrobotni.miasto = nieruchomosci.miasto INNER JOIN wynagrodzenie ON wynagrodzenie.miasto = bezrobotni.miasto INNER JOIN populacja ON populacja.miasto = bezrobotni.miasto WHERE bezrobotni.miasto = '${this.name}' GROUP BY populacja.liczba_ludnosci, populacja.procent_bezrobocia, populacja.ilosc_ofert_pracy, populacja.zatrudnieni_rolnictwo, populacja.zatrudnieni_uslugi, populacja.zatrudnieni_przemysl, populacja.liczba_ludnosci_stara, populacja.srednia_pensja_stara, populacja.srednia_pensja`;
         return sql;
     }
 
-    createResponseAllCities(results){
+    createResponseAllCities(results) {
         for (let index = 0; index < results.length; index++) {
-            let rolnictwo = results[index].zatrudnieni_rolnictwo/(results[index].zatrudnieni_rolnictwo + results[index].zatrudnieni_przemysl + results[index].zatrudnieni_uslugi) * 100;
-            let uslugi = results[index].zatrudnieni_uslugi/(results[index].zatrudnieni_rolnictwo + results[index].zatrudnieni_przemysl + results[index].zatrudnieni_uslugi) * 100;
-            let przemysl = results[index].zatrudnieni_przemysl/(results[index].zatrudnieni_rolnictwo + results[index].zatrudnieni_przemysl + results[index].zatrudnieni_uslugi) * 100;
-           
+            let rolnictwo = results[index].zatrudnieni_rolnictwo / (results[index].zatrudnieni_rolnictwo + results[index].zatrudnieni_przemysl + results[index].zatrudnieni_uslugi) * 100;
+            let uslugi = results[index].zatrudnieni_uslugi / (results[index].zatrudnieni_rolnictwo + results[index].zatrudnieni_przemysl + results[index].zatrudnieni_uslugi) * 100;
+            let przemysl = results[index].zatrudnieni_przemysl / (results[index].zatrudnieni_rolnictwo + results[index].zatrudnieni_przemysl + results[index].zatrudnieni_uslugi) * 100;
+            let wspMat = (results[index].liczba_ludnosci / results[index].liczba_ludnosci_stara) * (results[index].liczba_ludnosci / results[index].liczba_ludnosci_stara) * (results[index].srednia_pensja / results[index].srednia_pensja_stara) * (results[index].srednia_pensja / results[index].srednia_pensja_stara);
+
             let dataObject = {
                 miasto: results[index].miasto,
                 cenam2: results[index].cenam2,
@@ -28,24 +29,26 @@ class cityData {
                 wynagrodznieRelacja: results[index].wynagrodzenie_w_relacji,
                 pupulacja: results[index].liczba_ludnosci,
                 stopaBezrobocia: results[index].procent_bezrobocia,
-                ofertyPracyNaMieszkanca: results[index].ilosc_ofert_pracy/results[index].liczba_ludnosci,
+                ofertyPracyNaMieszkanca: results[index].ilosc_ofert_pracy / results[index].liczba_ludnosci,
                 zatrudnieniRolnictwo: rolnictwo,
                 zatrudnieniPrzemysl: przemysl,
-                zatrudnieniUslugi: uslugi
+                zatrudnieniUslugi: uslugi,
+                wspMatiego: wspMat
             }
 
             this.dataArray.push(dataObject);
-            
+
         }
         return this.dataArray;
     }
 
-    createResponse(results){
+    createResponse(results) {
         for (let index = 0; index < results.length; index++) {
-            let rolnictwo = results[index].zatrudnieni_rolnictwo/(results[index].zatrudnieni_rolnictwo + results[index].zatrudnieni_przemysl + results[index].zatrudnieni_uslugi) * 100;
-            let uslugi = results[index].zatrudnieni_uslugi/(results[index].zatrudnieni_rolnictwo + results[index].zatrudnieni_przemysl + results[index].zatrudnieni_uslugi) * 100;
-            let przemysl = results[index].zatrudnieni_przemysl/(results[index].zatrudnieni_rolnictwo + results[index].zatrudnieni_przemysl + results[index].zatrudnieni_uslugi) * 100;
-           
+            let rolnictwo = results[index].zatrudnieni_rolnictwo / (results[index].zatrudnieni_rolnictwo + results[index].zatrudnieni_przemysl + results[index].zatrudnieni_uslugi) * 100;
+            let uslugi = results[index].zatrudnieni_uslugi / (results[index].zatrudnieni_rolnictwo + results[index].zatrudnieni_przemysl + results[index].zatrudnieni_uslugi) * 100;
+            let przemysl = results[index].zatrudnieni_przemysl / (results[index].zatrudnieni_rolnictwo + results[index].zatrudnieni_przemysl + results[index].zatrudnieni_uslugi) * 100;
+            let wspMat = (results[index].liczba_ludnosci / results[index].liczba_ludnosci_stara) * (results[index].liczba_ludnosci / results[index].liczba_ludnosci_stara) * (results[index].srednia_pensja / results[index].srednia_pensja_stara) * (results[index].srednia_pensja / results[index].srednia_pensja_stara);
+
             let dataObject = {
                 cenam2: results[index].cenam2,
                 bezrobotni: results[index].bezrobotni,
@@ -53,14 +56,15 @@ class cityData {
                 wynagrodznieRelacja: results[index].wynagrodzenie_w_relacji,
                 pupulacja: results[index].liczba_ludnosci,
                 stopaBezrobocia: results[index].procent_bezrobocia,
-                ofertyPracyNaMieszkanca: results[index].ilosc_ofert_pracy/results[index].liczba_ludnosci,
+                ofertyPracyNaMieszkanca: results[index].ilosc_ofert_pracy / results[index].liczba_ludnosci,
                 zatrudnieniRolnictwo: rolnictwo,
                 zatrudnieniPrzemysl: przemysl,
-                zatrudnieniUslugi: uslugi
+                zatrudnieniUslugi: uslugi,
+                wspMatiego: wspMat
             }
 
             this.dataArray.push(dataObject);
-            
+
         }
         return this.dataArray;
     }
