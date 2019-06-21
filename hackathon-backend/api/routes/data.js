bb7db4e89b5bf3d54f073981c23d1cb1091ecc4a
krzysztof <krzysztof.brzoska@ibm.com>
2019-06-22 00:54:54 +0200

    add new value

--- hackathon-backend/api/routes/data.js
+++ hackathon-backend/api/routes/data.js
@@ -11,6 +11,7 @@ router.get('/', (req, res, next) => {
     const jobsInfo = new jobsOffersInfo(100);
     connection.query(jobsInfo.createSelectQuery(), function (error, results, fields) {
         if (error) throw error;
+        
         res.status(200).json({
             data: results
         });
@@ -45,9 +46,24 @@ router.get('/allData/:city', (req, res, next) => {
     const cityData = new CityData(req.params.city);
     connection.query(cityData.createSelectQuery(), function (error, results, fields) {
         if (error) throw error;
+        var dataArray = [];
+        for (let index = 0; index < results.length; index++) {
+            let dataObject = {
+                cenam2: results[index].cenam2,
+                bezrobotni: results[index].bezrobotni,
+                wynagrodzenie: results[index].wynagrodzenie_brutto,
+                wynagrodznieRelacja: results[index].wynagrodzenie_w_relacji,
+                pupulacja: results[index].liczba_ludnosci,
+                stopaBezrobocia: results[index].procent_bezrobocia,
+                ofertyPracyNaMieszkanca: results[index].ilosc_ofert_pracy/results[index].liczba_ludnosci
+            }
+
+            dataArray.push(dataObject);
+            
+        }
         res.status(200).json({
             message: "Pobrales wszytskie dane o " + req.params.city,
-            data: results,
+            data: dataArray,
             yourValue: req.params.city
         });
     });
@@ -57,9 +73,27 @@ router.get('/allCities', (req, res, next) => {
     const cityData = new CityData(null);
     connection.query(cityData.createSelectQueryAllCities(), function (error, results, fields) {
         if (error) throw error;
+        var dataArray = [];
+        // console.log(results)
+        for (let index = 0; index < results.length; index++) {
+            let dataObject = {
+                miasto: results[index].miasto,
+                cenam2: results[index].cenam2,
+                bezrobotni: results[index].bezrobotni,
+                wynagrodzenie: results[index].wynagrodzenie_brutto,
+                wynagrodznieRelacja: results[index].wynagrodzenie_w_relacji,
+                pupulacja: results[index].liczba_ludnosci,
+                stopaBezrobocia: results[index].procent_bezrobocia,
+                ofertyPracyNaMieszkanca: results[index].ilosc_ofert_pracy/results[index].liczba_ludnosci
+            }
+
+            dataArray.push(dataObject);
+            
+        }
+        // console.log(dataArray);
         res.status(200).json({
             message: "Pobrales dane o wszytskich miastach",
-            data: results
+            data: dataArray
         });
     });
 });
--- hackathon-backend/index.js
+++ hackathon-backend/index.js
@@ -1,49 +1,50 @@
 
     const connection = require('./api/db/dbconnect');
-    var data = `"RYNEK NIERUCHOMOŚCI";"RYNKOWA SPRZEDAŻ LOKALI MIESZKALNYCH";"Mediana cen za 1 m2 lokali mieszkalnych sprzedanych w ramach transakcji rynkowych (Wymiary: Transakcje rynkowe; Powierzchnia użytkowa lokali mieszkalnych)";"ogółem";"ogółem";"";"zł";5897;
-    "RYNEK NIERUCHOMOŚCI";"RYNKOWA SPRZEDAŻ LOKALI MIESZKALNYCH";"Mediana cen za 1 m2 lokali mieszkalnych sprzedanych w ramach transakcji rynkowych (Wymiary: Transakcje rynkowe; Powierzchnia użytkowa lokali mieszkalnych)";"ogółem";"do 40 m2";"";"zł";6051;
-    "RYNEK NIERUCHOMOŚCI";"RYNKOWA SPRZEDAŻ LOKALI MIESZKALNYCH";"Mediana cen za 1 m2 lokali mieszkalnych sprzedanych w ramach transakcji rynkowych (Wymiary: Transakcje rynkowe; Powierzchnia użytkowa lokali mieszkalnych)";"ogółem";"od 40,1 do 60 m2";"";"zł";5920;
-    "RYNEK NIERUCHOMOŚCI";"RYNKOWA SPRZEDAŻ LOKALI MIESZKALNYCH";"Mediana cen za 1 m2 lokali mieszkalnych sprzedanych w ramach transakcji rynkowych (Wymiary: Transakcje rynkowe; Powierzchnia użytkowa lokali mieszkalnych)";"ogółem";"od 60,1 do 80 m2";"";"zł";5652;
-    "RYNEK NIERUCHOMOŚCI";"RYNKOWA SPRZEDAŻ LOKALI MIESZKALNYCH";"Mediana cen za 1 m2 lokali mieszkalnych sprzedanych w ramach transakcji rynkowych (Wymiary: Transakcje rynkowe; Powierzchnia użytkowa lokali mieszkalnych)";"ogółem";"od 80,1 m2";"";"zł";4841;
-    "RYNEK NIERUCHOMOŚCI";"RYNKOWA SPRZEDAŻ LOKALI MIESZKALNYCH";"Mediana cen za 1 m2 lokali mieszkalnych sprzedanych w ramach transakcji rynkowych (Wymiary: Transakcje rynkowe; Powierzchnia użytkowa lokali mieszkalnych)";"rynek pierwotny";"ogółem";"";"zł";6055;
-    "RYNEK NIERUCHOMOŚCI";"RYNKOWA SPRZEDAŻ LOKALI MIESZKALNYCH";"Mediana cen za 1 m2 lokali mieszkalnych sprzedanych w ramach transakcji rynkowych (Wymiary: Transakcje rynkowe; Powierzchnia użytkowa lokali mieszkalnych)";"rynek pierwotny";"do 40 m2";"";"zł";6081;
-    "RYNEK NIERUCHOMOŚCI";"RYNKOWA SPRZEDAŻ LOKALI MIESZKALNYCH";"Mediana cen za 1 m2 lokali mieszkalnych sprzedanych w ramach transakcji rynkowych (Wymiary: Transakcje rynkowe; Powierzchnia użytkowa lokali mieszkalnych)";"rynek pierwotny";"od 40,1 do 60 m2";"";"zł";6089;
-    "RYNEK NIERUCHOMOŚCI";"RYNKOWA SPRZEDAŻ LOKALI MIESZKALNYCH";"Mediana cen za 1 m2 lokali mieszkalnych sprzedanych w ramach transakcji rynkowych (Wymiary: Transakcje rynkowe; Powierzchnia użytkowa lokali mieszkalnych)";"rynek pierwotny";"od 60,1 do 80 m2";"";"zł";5977;
-    "RYNEK NIERUCHOMOŚCI";"RYNKOWA SPRZEDAŻ LOKALI MIESZKALNYCH";"Mediana cen za 1 m2 lokali mieszkalnych sprzedanych w ramach transakcji rynkowych (Wymiary: Transakcje rynkowe; Powierzchnia użytkowa lokali mieszkalnych)";"rynek pierwotny";"od 80,1 m2";"";"zł";5854;
-    "RYNEK NIERUCHOMOŚCI";"RYNKOWA SPRZEDAŻ LOKALI MIESZKALNYCH";"Mediana cen za 1 m2 lokali mieszkalnych sprzedanych w ramach transakcji rynkowych (Wymiary: Transakcje rynkowe; Powierzchnia użytkowa lokali mieszkalnych)";"rynek wtórny";"ogółem";"";"zł";5400;
-    "RYNEK NIERUCHOMOŚCI";"RYNKOWA SPRZEDAŻ LOKALI MIESZKALNYCH";"Mediana cen za 1 m2 lokali mieszkalnych sprzedanych w ramach transakcji rynkowych (Wymiary: Transakcje rynkowe; Powierzchnia użytkowa lokali mieszkalnych)";"rynek wtórny";"do 40 m2";"";"zł";5992;
-    "RYNEK NIERUCHOMOŚCI";"RYNKOWA SPRZEDAŻ LOKALI MIESZKALNYCH";"Mediana cen za 1 m2 lokali mieszkalnych sprzedanych w ramach transakcji rynkowych (Wymiary: Transakcje rynkowe; Powierzchnia użytkowa lokali mieszkalnych)";"rynek wtórny";"od 40,1 do 60 m2";"";"zł";5474;
-    "RYNEK NIERUCHOMOŚCI";"RYNKOWA SPRZEDAŻ LOKALI MIESZKALNYCH";"Mediana cen za 1 m2 lokali mieszkalnych sprzedanych w ramach transakcji rynkowych (Wymiary: Transakcje rynkowe; Powierzchnia użytkowa lokali mieszkalnych)";"rynek wtórny";"od 60,1 do 80 m2";"";"zł";4931;
-    "RYNEK NIERUCHOMOŚCI";"RYNKOWA SPRZEDAŻ LOKALI MIESZKALNYCH";"Mediana cen za 1 m2 lokali mieszkalnych sprzedanych w ramach transakcji rynkowych (Wymiary: Transakcje rynkowe; Powierzchnia użytkowa lokali mieszkalnych)";"rynek wtórny";"od 80,1 m2";"";"zł";4212;`
+    var data = `"RYNEK NIERUCHOMOŚCI";"RYNKOWA SPRZEDAŻ LOKALI MIESZKALNYCH";"Mediana cen za 1 m2 lokali mieszkalnych sprzedanych w ramach transakcji rynkowych (Wymiary: Transakcje rynkowe; Powierzchnia użytkowa lokali mieszkalnych)";"ogółem";"ogółem";"";"zł";6246;
+    "RYNEK NIERUCHOMOŚCI";"RYNKOWA SPRZEDAŻ LOKALI MIESZKALNYCH";"Mediana cen za 1 m2 lokali mieszkalnych sprzedanych w ramach transakcji rynkowych (Wymiary: Transakcje rynkowe; Powierzchnia użytkowa lokali mieszkalnych)";"ogółem";"do 40 m2";"";"zł";6431;
+    "RYNEK NIERUCHOMOŚCI";"RYNKOWA SPRZEDAŻ LOKALI MIESZKALNYCH";"Mediana cen za 1 m2 lokali mieszkalnych sprzedanych w ramach transakcji rynkowych (Wymiary: Transakcje rynkowe; Powierzchnia użytkowa lokali mieszkalnych)";"ogółem";"od 40,1 do 60 m2";"";"zł";6135;
+    "RYNEK NIERUCHOMOŚCI";"RYNKOWA SPRZEDAŻ LOKALI MIESZKALNYCH";"Mediana cen za 1 m2 lokali mieszkalnych sprzedanych w ramach transakcji rynkowych (Wymiary: Transakcje rynkowe; Powierzchnia użytkowa lokali mieszkalnych)";"ogółem";"od 60,1 do 80 m2";"";"zł";6109;
+    "RYNEK NIERUCHOMOŚCI";"RYNKOWA SPRZEDAŻ LOKALI MIESZKALNYCH";"Mediana cen za 1 m2 lokali mieszkalnych sprzedanych w ramach transakcji rynkowych (Wymiary: Transakcje rynkowe; Powierzchnia użytkowa lokali mieszkalnych)";"ogółem";"od 80,1 m2";"";"zł";6544;
+    "RYNEK NIERUCHOMOŚCI";"RYNKOWA SPRZEDAŻ LOKALI MIESZKALNYCH";"Mediana cen za 1 m2 lokali mieszkalnych sprzedanych w ramach transakcji rynkowych (Wymiary: Transakcje rynkowe; Powierzchnia użytkowa lokali mieszkalnych)";"rynek pierwotny";"ogółem";"";"zł";6293;
+    "RYNEK NIERUCHOMOŚCI";"RYNKOWA SPRZEDAŻ LOKALI MIESZKALNYCH";"Mediana cen za 1 m2 lokali mieszkalnych sprzedanych w ramach transakcji rynkowych (Wymiary: Transakcje rynkowe; Powierzchnia użytkowa lokali mieszkalnych)";"rynek pierwotny";"do 40 m2";"";"zł";6346;
+    "RYNEK NIERUCHOMOŚCI";"RYNKOWA SPRZEDAŻ LOKALI MIESZKALNYCH";"Mediana cen za 1 m2 lokali mieszkalnych sprzedanych w ramach transakcji rynkowych (Wymiary: Transakcje rynkowe; Powierzchnia użytkowa lokali mieszkalnych)";"rynek pierwotny";"od 40,1 do 60 m2";"";"zł";6195;
+    "RYNEK NIERUCHOMOŚCI";"RYNKOWA SPRZEDAŻ LOKALI MIESZKALNYCH";"Mediana cen za 1 m2 lokali mieszkalnych sprzedanych w ramach transakcji rynkowych (Wymiary: Transakcje rynkowe; Powierzchnia użytkowa lokali mieszkalnych)";"rynek pierwotny";"od 60,1 do 80 m2";"";"zł";6298;
+    "RYNEK NIERUCHOMOŚCI";"RYNKOWA SPRZEDAŻ LOKALI MIESZKALNYCH";"Mediana cen za 1 m2 lokali mieszkalnych sprzedanych w ramach transakcji rynkowych (Wymiary: Transakcje rynkowe; Powierzchnia użytkowa lokali mieszkalnych)";"rynek pierwotny";"od 80,1 m2";"";"zł";6998;
+    "RYNEK NIERUCHOMOŚCI";"RYNKOWA SPRZEDAŻ LOKALI MIESZKALNYCH";"Mediana cen za 1 m2 lokali mieszkalnych sprzedanych w ramach transakcji rynkowych (Wymiary: Transakcje rynkowe; Powierzchnia użytkowa lokali mieszkalnych)";"rynek wtórny";"ogółem";"";"zł";6159;
+    "RYNEK NIERUCHOMOŚCI";"RYNKOWA SPRZEDAŻ LOKALI MIESZKALNYCH";"Mediana cen za 1 m2 lokali mieszkalnych sprzedanych w ramach transakcji rynkowych (Wymiary: Transakcje rynkowe; Powierzchnia użytkowa lokali mieszkalnych)";"rynek wtórny";"do 40 m2";"";"zł";6557;
+    "RYNEK NIERUCHOMOŚCI";"RYNKOWA SPRZEDAŻ LOKALI MIESZKALNYCH";"Mediana cen za 1 m2 lokali mieszkalnych sprzedanych w ramach transakcji rynkowych (Wymiary: Transakcje rynkowe; Powierzchnia użytkowa lokali mieszkalnych)";"rynek wtórny";"od 40,1 do 60 m2";"";"zł";6005;
+    "RYNEK NIERUCHOMOŚCI";"RYNKOWA SPRZEDAŻ LOKALI MIESZKALNYCH";"Mediana cen za 1 m2 lokali mieszkalnych sprzedanych w ramach transakcji rynkowych (Wymiary: Transakcje rynkowe; Powierzchnia użytkowa lokali mieszkalnych)";"rynek wtórny";"od 60,1 do 80 m2";"";"zł";5903;
+    "RYNEK NIERUCHOMOŚCI";"RYNKOWA SPRZEDAŻ LOKALI MIESZKALNYCH";"Mediana cen za 1 m2 lokali mieszkalnych sprzedanych w ramach transakcji rynkowych (Wymiary: Transakcje rynkowe; Powierzchnia użytkowa lokali mieszkalnych)";"rynek wtórny";"od 80,1 m2";"";"zł";5974;`
     var counter = 0;
     var ceny = [];
     var cena = '';
     module.exports = function wykonaj(){
-        for (let index = 0; index < data.length; index++) {
-            if (data[index] == ';'){
-                counter++;
-                continue;
-            } 
+        // for (let index = 0; index < data.length; index++) {
+        //     if (data[index] == ';'){
+        //         counter++;
+        //         continue;
+        //     } 
             
-            if (counter == 8) {
-                cena += data[index];
-                console.log(cena);
-                if (data[index +1] == ';') {
-                    ceny.push(cena);
-                    counter = -1;
+        //     if (counter == 8) {
+        //         cena += data[index];
+        //         console.log(cena);
+        //         if (data[index +1] == ';') {
+        //             ceny.push(cena);
+        //             counter = -1;
                     
-                    var sql = `insert into nieruchomosci (miasto,cena) Values ('Poznań',${cena})`;
-                    console.log(sql)
-                    connection.query(sql, function (err, result) {
-                        if (err) throw err;
-                        console.log("1 record inserted");
-                    });
-                    cena = ''
-                }
+        //             var sql = `insert into nieruchomosci (miasto,cena) Values ('Kraków',${cena})`;
+        //             console.log(sql)
+        //             connection.query(sql, function (err, result) {
+        //                 if (err) throw err;
+        //                 console.log("1 record inserted");
+        //             });
+        //             cena = ''
+        //         }
                 
-            }
+        //     }
             
-        }
+        // }
+      
     }
     
     
