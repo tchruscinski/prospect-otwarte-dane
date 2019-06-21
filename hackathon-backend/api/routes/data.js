839c5ddb3b332b26e45f27cd4d7f9b26533fce02
krzysztof <krzysztof.brzoska@ibm.com>
2019-06-22 01:34:12 +0200

    new data in response

--- hackathon-backend/api/routes/data.js
+++ hackathon-backend/api/routes/data.js
@@ -46,24 +46,9 @@ router.get('/allData/:city', (req, res, next) => {
     const cityData = new CityData(req.params.city);
     connection.query(cityData.createSelectQuery(), function (error, results, fields) {
         if (error) throw error;
-        var dataArray = [];
-        for (let index = 0; index < results.length; index++) {
-            let dataObject = {
-                cenam2: results[index].cenam2,
-                bezrobotni: results[index].bezrobotni,
-                wynagrodzenie: results[index].wynagrodzenie_brutto,
-                wynagrodznieRelacja: results[index].wynagrodzenie_w_relacji,
-                pupulacja: results[index].liczba_ludnosci,
-                stopaBezrobocia: results[index].procent_bezrobocia,
-                ofertyPracyNaMieszkanca: results[index].ilosc_ofert_pracy/results[index].liczba_ludnosci
-            }
-
-            dataArray.push(dataObject);
-            
-        }
         res.status(200).json({
             message: "Pobrales wszytskie dane o " + req.params.city,
-            data: dataArray,
+            data: cityData.createResponse(results),
             yourValue: req.params.city
         });
     });
@@ -73,27 +58,10 @@ router.get('/allCities', (req, res, next) => {
     const cityData = new CityData(null);
     connection.query(cityData.createSelectQueryAllCities(), function (error, results, fields) {
         if (error) throw error;
-        var dataArray = [];
-        // console.log(results)
-        for (let index = 0; index < results.length; index++) {
-            let dataObject = {
-                miasto: results[index].miasto,
-                cenam2: results[index].cenam2,
-                bezrobotni: results[index].bezrobotni,
-                wynagrodzenie: results[index].wynagrodzenie_brutto,
-                wynagrodznieRelacja: results[index].wynagrodzenie_w_relacji,
-                pupulacja: results[index].liczba_ludnosci,
-                stopaBezrobocia: results[index].procent_bezrobocia,
-                ofertyPracyNaMieszkanca: results[index].ilosc_ofert_pracy/results[index].liczba_ludnosci
-            }
-
-            dataArray.push(dataObject);
-            
-        }
         // console.log(dataArray);
         res.status(200).json({
             message: "Pobrales dane o wszytskich miastach",
-            data: dataArray
+            data: cityData.createResponseAllCities(results)
         });
     });
 });
--- hackathon-backend/api/services/cityData.js
+++ hackathon-backend/api/services/cityData.js
@@ -1,16 +1,68 @@
 class cityData {
     constructor(name) {
         this.name = name;
+        this.dataArray = [];
     }
 
     createSelectQueryAllCities(){
-        let sql = `SELECT nieruchomosci.miasto, AVG(nieruchomosci.cena) AS cenam2, SUM(bezrobotni.ilosc) AS bezrobotni, wynagrodzenie.wynagrodzenie_brutto, wynagrodzenie.wynagrodzenie_w_relacji, populacja.liczba_ludnosci, populacja.procent_bezrobocia, populacja.ilosc_ofert_pracy FROM bezrobotni INNER JOIN nieruchomosci ON bezrobotni.miasto = nieruchomosci.miasto INNER JOIN wynagrodzenie ON wynagrodzenie.miasto = bezrobotni.miasto INNER JOIN populacja ON populacja.miasto = bezrobotni.miasto GROUP BY populacja.liczba_ludnosci, wynagrodzenie.wynagrodzenie_brutto, wynagrodzenie.wynagrodzenie_w_relacji, nieruchomosci.miasto, populacja.procent_bezrobocia, populacja.ilosc_ofert_pracy`;
+        let sql = `SELECT nieruchomosci.miasto, AVG(nieruchomosci.cena) AS cenam2, SUM(bezrobotni.ilosc) AS bezrobotni, wynagrodzenie.wynagrodzenie_brutto, wynagrodzenie.wynagrodzenie_w_relacji, populacja.liczba_ludnosci, populacja.procent_bezrobocia, populacja.ilosc_ofert_pracy, populacja.zatrudnieni_rolnictwo, populacja.zatrudnieni_uslugi, populacja.zatrudnieni_przemysl FROM bezrobotni INNER JOIN nieruchomosci ON bezrobotni.miasto = nieruchomosci.miasto INNER JOIN wynagrodzenie ON wynagrodzenie.miasto = bezrobotni.miasto INNER JOIN populacja ON populacja.miasto = bezrobotni.miasto GROUP BY populacja.liczba_ludnosci, wynagrodzenie.wynagrodzenie_brutto, wynagrodzenie.wynagrodzenie_w_relacji, nieruchomosci.miasto, populacja.procent_bezrobocia, populacja.ilosc_ofert_pracy, populacja.zatrudnieni_rolnictwo, populacja.zatrudnieni_uslugi, populacja.zatrudnieni_przemysl`;
         return sql;
     }
 
     createSelectQuery(){
-        let sql = `SELECT AVG(nieruchomosci.cena) AS cenam2, SUM(bezrobotni.ilosc) AS bezrobotni, wynagrodzenie.wynagrodzenie_brutto, wynagrodzenie.wynagrodzenie_w_relacji, populacja.liczba_ludnosci, populacja.procent_bezrobocia, populacja.ilosc_ofert_pracy FROM bezrobotni INNER JOIN nieruchomosci ON bezrobotni.miasto = nieruchomosci.miasto INNER JOIN wynagrodzenie ON wynagrodzenie.miasto = bezrobotni.miasto INNER JOIN populacja ON populacja.miasto = bezrobotni.miasto WHERE bezrobotni.miasto = '${this.name}' GROUP BY populacja.liczba_ludnosci, populacja.procent_bezrobocia, populacja.ilosc_ofert_pracy`;
+        let sql = `SELECT AVG(nieruchomosci.cena) AS cenam2, SUM(bezrobotni.ilosc) AS bezrobotni, wynagrodzenie.wynagrodzenie_brutto, wynagrodzenie.wynagrodzenie_w_relacji, populacja.liczba_ludnosci, populacja.procent_bezrobocia, populacja.ilosc_ofert_pracy, populacja.zatrudnieni_rolnictwo, populacja.zatrudnieni_uslugi, populacja.zatrudnieni_przemysl FROM bezrobotni INNER JOIN nieruchomosci ON bezrobotni.miasto = nieruchomosci.miasto INNER JOIN wynagrodzenie ON wynagrodzenie.miasto = bezrobotni.miasto INNER JOIN populacja ON populacja.miasto = bezrobotni.miasto WHERE bezrobotni.miasto = '${this.name}' GROUP BY populacja.liczba_ludnosci, populacja.procent_bezrobocia, populacja.ilosc_ofert_pracy, populacja.zatrudnieni_rolnictwo, populacja.zatrudnieni_uslugi, populacja.zatrudnieni_przemysl`;
         return sql;
+    }
+
+    createResponseAllCities(results){
+        for (let index = 0; index < results.length; index++) {
+            let rolnictwo = results[index].zatrudnieni_rolnictwo/(results[index].zatrudnieni_rolnictwo + results[index].zatrudnieni_przemysl + results[index].zatrudnieni_uslugi) * 100;
+            let uslugi = results[index].zatrudnieni_uslugi/(results[index].zatrudnieni_rolnictwo + results[index].zatrudnieni_przemysl + results[index].zatrudnieni_uslugi) * 100;
+            let przemysl = results[index].zatrudnieni_przemysl/(results[index].zatrudnieni_rolnictwo + results[index].zatrudnieni_przemysl + results[index].zatrudnieni_uslugi) * 100;
+           
+            let dataObject = {
+                miasto: results[index].miasto,
+                cenam2: results[index].cenam2,
+                bezrobotni: results[index].bezrobotni,
+                wynagrodzenie: results[index].wynagrodzenie_brutto,
+                wynagrodznieRelacja: results[index].wynagrodzenie_w_relacji,
+                pupulacja: results[index].liczba_ludnosci,
+                stopaBezrobocia: results[index].procent_bezrobocia,
+                ofertyPracyNaMieszkanca: results[index].ilosc_ofert_pracy/results[index].liczba_ludnosci,
+                zatrudnieniRolnictwo: rolnictwo,
+                zatrudnieniPrzemysl: przemysl,
+                zatrudnieniUslugi: uslugi
+            }
+
+            this.dataArray.push(dataObject);
+            
+        }
+        return this.dataArray;
+    }
+
+    createResponse(results){
+        for (let index = 0; index < results.length; index++) {
+            let rolnictwo = results[index].zatrudnieni_rolnictwo/(results[index].zatrudnieni_rolnictwo + results[index].zatrudnieni_przemysl + results[index].zatrudnieni_uslugi) * 100;
+            let uslugi = results[index].zatrudnieni_uslugi/(results[index].zatrudnieni_rolnictwo + results[index].zatrudnieni_przemysl + results[index].zatrudnieni_uslugi) * 100;
+            let przemysl = results[index].zatrudnieni_przemysl/(results[index].zatrudnieni_rolnictwo + results[index].zatrudnieni_przemysl + results[index].zatrudnieni_uslugi) * 100;
+           
+            let dataObject = {
+                cenam2: results[index].cenam2,
+                bezrobotni: results[index].bezrobotni,
+                wynagrodzenie: results[index].wynagrodzenie_brutto,
+                wynagrodznieRelacja: results[index].wynagrodzenie_w_relacji,
+                pupulacja: results[index].liczba_ludnosci,
+                stopaBezrobocia: results[index].procent_bezrobocia,
+                ofertyPracyNaMieszkanca: results[index].ilosc_ofert_pracy/results[index].liczba_ludnosci,
+                zatrudnieniRolnictwo: rolnictwo,
+                zatrudnieniPrzemysl: przemysl,
+                zatrudnieniUslugi: uslugi
+            }
+
+            this.dataArray.push(dataObject);
+            
+        }
+        return this.dataArray;
     }
 
 }
